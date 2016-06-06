b7a9a7ccf0b73bd2224352887252b2d281c5cdd2
oromis95 <dtropeano@hotmail.it>
2016-06-06 12:35:19 +0200

    bozza add product admin

--- www.scarpe.it/WebContent/js/admin.js
+++ www.scarpe.it/WebContent/js/admin.js
@@ -1,10 +1,8 @@
-function showStuff(selectFunction){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange=function() {
-	    if (xhttp.readyState == 4 && xhttp.status == 200) {
-	     alert("tuto");
-	    }
-	  };
-	  xhttp.open("GET", "management.jsp?funzione="+selectFunction, true);
-	  xhttp.send();
-}
+
+
+function showStuff(string){ 
+
+var eid = string; 
+alert("mah")
+$("#div1").load(eid); 
+} 
---
+++ www.scarpe.it/WebContent/page2.jsp
@@ -0,0 +1,4 @@
+<div style="z-index:1" >
+<b>Page2.jsp</b><br>
+<i> This is the content of Page2.jsp page</i>
+</div>
