f183aed8c5c61034fd79634513192d0e1d412d5f
izio7 <iziostreaming@gmail.com>
2016-06-15 16:44:39 +0200

    Aggiunta interfaccia costanti URL applicazione.

---
+++ www.scarpe.it/src/constants/Constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public interface Constants {
+	public static final String protocol = "http://";
+	public static final String host = "127.0.0.1";
+	public static final String port = ":80";
+	public static final String application ="/www.scarpe.it";
+	public static final String rootURL = protocol + host + port + application;
+}
--- www.scarpe.it/src/utilities/xml/EsportaProdotti.java
+++ www.scarpe.it/src/utilities/xml/EsportaProdotti.java
@@ -8,24 +8,24 @@ import org.jdom2.Element;
 import database.Database;
 
 public class EsportaProdotti {
-	
-	public static Element makeExportShoesXML() throws SQLException{
+
+	public static Element makeExportShoesXML() throws SQLException {
 		Database.openConnection();
-		
+
 		String countItems = "SELECT COUNT(*) FROM lisca.scarpe";
 		String items = "SELECT * FROM lisca.scarpe";
 		String images = "SELECT * FROM lisca.immagini";
 		String countImages = "SELECT COUNT(*) FROM lisca.immagini";
 		String details = "SELECT * FROM lisca.dettagli";
 		String countDetails = "SELECT COUNT(*) FROM lisca.dettagli";
-		
+
 		ResultSet numOfItems = null;
 		ResultSet itemsToExport = null;
 		ResultSet imageToExport = null;
 		ResultSet numOfImages = null;
 		ResultSet detailsToExport = null;
 		ResultSet numOfDetails = null;
-		
+
 		try {
 			numOfItems = Database.executeQuery(countItems);
 			itemsToExport = Database.executeQuery(items);
@@ -36,27 +36,27 @@ public class EsportaProdotti {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		if(numOfItems != null && items != null){
-			if(numOfImages != null && imageToExport != null){
+
+		if (numOfItems != null && items != null) {
+			if (numOfImages != null && imageToExport != null) {
 				numOfImages.next();
 				hasImage = true;
 			}
-			
-			if(numOfDetails != null && detailsToExport != null){
+
+			if (numOfDetails != null && detailsToExport != null) {
 				numOfDetails.next();
 				hasDetails = true;
 			}
-			
-			scarpe = new Element("scarpe"); //root element
-			
+
+			scarpe = new Element("scarpe"); // root element
+
 			numOfItems.next(); // mi posiziono sul risultato
-			
-			for(int i = 1; i <= numOfItems.getInt(1); i++){
+
+			for (int i = 1; i <= numOfItems.getInt(1); i++) {
 				itemsToExport.next(); // mi posiziono sulla riga successiva
-				
+
 				scarpa = new Element("scarpa");
-				
+
 				idScarpe = new Element("idScarpe");
 				marca = new Element("marca");
 				modello = new Element("modello");
@@ -68,7 +68,7 @@ public class EsportaProdotti {
 				descrizione = new Element("descrizione");
 				immagini = new Element("immagini");
 				dettagli = new Element("dettagli");
-				
+
 				idScarpe.addContent(String.valueOf(itemsToExport.getInt("idScarpe")));
 				marca.addContent(itemsToExport.getString("marca"));
 				modello.addContent(itemsToExport.getString("modello"));
@@ -78,7 +78,7 @@ public class EsportaProdotti {
 				scorta_minima.addContent(String.valueOf(itemsToExport.getInt("scorta_minima")));
 				alt.addContent(itemsToExport.getString("alt"));
 				descrizione.addContent(itemsToExport.getString("descrizione"));
-				
+
 				scarpa.addContent(idScarpe);
 				scarpa.addContent(marca);
 				scarpa.addContent(modello);
@@ -88,31 +88,31 @@ public class EsportaProdotti {
 				scarpa.addContent(scorta_minima);
 				scarpa.addContent(alt);
 				scarpa.addContent(descrizione);
-				
-				if(hasImage){
-					for(int j = 1; j <= numOfImages.getInt(1); j++){
-						
+
+				if (hasImage) {
+					for (int j = 1; j <= numOfImages.getInt(1); j++) {
+
 						boolean isImage = imageToExport.next();
-						if(isImage && itemsToExport.getInt("idScarpe") == imageToExport.getInt("scarpa")){
+						if (isImage && itemsToExport.getInt("idScarpe") == imageToExport.getInt("scarpa")) {
 							url = new Element("url");
 							System.out.println(imageToExport.getString("url"));
 							url.addContent(imageToExport.getString("url"));
 							immagini.addContent(url);
-						}	
+						}
 					}
 					scarpa.addContent(immagini);
 				}
-				
+
 				imageToExport.beforeFirst();
-				
-				if(hasDetails){
-					for(int z = 1; z <= numOfDetails.getInt(1); z++){
+
+				if (hasDetails) {
+					for (int z = 1; z <= numOfDetails.getInt(1); z++) {
 						boolean isDetail = detailsToExport.next();
-						if(isDetail && itemsToExport.getInt("idScarpe") == detailsToExport.getInt("scarpa")){
+						if (isDetail && itemsToExport.getInt("idScarpe") == detailsToExport.getInt("scarpa")) {
 							dettaglio = new Element("dettaglio");
 							intestazione = new Element("intestazione");
 							corpo = new Element("corpo");
-						
+
 							intestazione.addContent(detailsToExport.getString("intestazione"));
 							corpo.addContent(detailsToExport.getString("corpo"));
 							dettaglio.addContent(intestazione);
@@ -122,28 +122,28 @@ public class EsportaProdotti {
 					}
 					detailsToExport.beforeFirst();
 					scarpa.addContent(dettagli);
-				}	
-				scarpe.addContent(scarpa);// aggiungo a root	
+				}
+				scarpe.addContent(scarpa);// aggiungo a root
 			}
 		}
 		return scarpe;
 	}
-	
-	public static String getDtd(){
+
+	public static String getDtd() {
 		return dtd;
 	}
-	
+
 	private static Element scarpe;
 	private static Element scarpa;
 	private static Element idScarpe, marca, modello, prezzo_vendita, prezzo_acquisto;
 	private static Element quantitaDisp, scorta_minima, immagini, url;
 	private static Element alt, descrizione, dettagli, dettaglio, intestazione, corpo;
 	private static boolean hasImage, hasDetails;
-	
+
 	private static String dtd;
-	
-	static{	
-		dtd="http://127.0.0.1:8080/www.scarpe.it/dtd/scarpe.dtd";
+
+	static {
+		dtd = constants.Constants.rootURL + "/dtd/scarpe.dtd";
 		hasImage = false;
 		hasDetails = false;
 	}
